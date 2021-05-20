cd3eb758a2bc1f4fc47f34880a6db1584bca870c
TMc42 <tmclean1212@gmail.com>
2021-05-20 11:50:14 +0200

    Setting look and feel and color scheme

--- src/window.java
+++ src/window.java
@@ -6,6 +6,20 @@ public class Window {
         JFrame frame = new JFrame("McNote");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        try {
+            UIManager.put("nimbusBase", new Color(000000));
+            UIManager.put("nimbusBlueGrey", new Color(000000));
+
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())){
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e){
+            // Set backup look and feel
+        }
+
         JLabel emptyLabel = new JLabel("");
         emptyLabel.setPreferredSize(new Dimension(600, 600));
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
