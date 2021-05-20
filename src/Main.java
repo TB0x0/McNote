fdb91688eec88d6a46cb0f5df81773773bc2f3be
TMc42 <tmclean1212@gmail.com>
2021-05-20 14:39:09 +0200

    Began port to javafx
    Cleaned up logic, added comments, added basic menu layout and close functionality

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,61 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args){
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Window nWindow = new Window();
-                nWindow.createWindow();
-            }
+
+public class Main extends Application  {
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("McNote");
+
+        // Menu
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem newDoc = new MenuItem("New");
+        MenuItem openDoc = new MenuItem("Open");
+        MenuItem saveDoc = new MenuItem("Save");
+        MenuItem exitDoc = new MenuItem("Exit");
+        fileMenu.getItems().add(newDoc);
+        fileMenu.getItems().add(openDoc);
+        fileMenu.getItems().add(saveDoc);
+        fileMenu.getItems().add(exitDoc);
+
+        menuBar.getMenus().add(fileMenu);
+
+        // Text Editor
+        TextArea textArea = new TextArea();
+        textArea.setPrefHeight(600);
+        textArea.setPrefWidth(600);
+
+        // Events
+        newDoc.setOnAction(e -> {
+            System.out.println("This will start a new document");
         });
+        openDoc.setOnAction(e -> {
+            System.out.println("This will open a document");
+        });
+        saveDoc.setOnAction(e -> {
+            System.out.println("This will save a document");
+        });
+        exitDoc.setOnAction(e -> {
+            stage.close();
+        });
+
+        // Scene
+        VBox vbox = new VBox(menuBar);
+        vbox.getChildren().add(textArea);
+
+        Scene scene = new Scene(vbox, 600, 600);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
--- src/window.java
+++ src/window.java
@@ -1,151 +1,25 @@
-import javax.swing.*;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.text.Font;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import java.awt.Color;
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
 
-public class Window implements ActionListener {
-    JTextArea textArea;
-    JFrame frame = new JFrame("McNote");
-    public void createWindow(){
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class Window{
+    public void start(Stage stage){
+        Font font = Font.font("Times New Roman", 12);
+        TextArea area = new TextArea();
+        area.setPrefHeight(400);
+        area.setPrefWidth(400);
 
-        try {
-            //UIManager.put("nimbusBase", new Color(000000));
-            //UIManager.put("nimbusBlueGrey", new Color(000000));
-
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())){
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (Exception e){
-           // UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-            //javax.swing.plaf.metal.MetalLookAndFeel.setCurrentTheme(new OceanTheme());
-        }
-
-        textArea = new JTextArea(500,600);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        textArea.setEditable(true);
-
-        JMenuBar menuBar = new JMenuBar();
-
-        JMenu subMenu1 = new JMenu("File");
-        JMenuItem item1 = new JMenuItem("New");
-        JMenuItem item2 = new JMenuItem("Open");
-        JMenuItem item3 = new JMenuItem("Save");
-        JMenuItem item9 = new JMenuItem("Print");
-        item1.addActionListener(this);
-        item2.addActionListener(this);
-        item3.addActionListener(this);
-        item9.addActionListener(this);
-        subMenu1.add(item1);
-        subMenu1.add(item2);
-        subMenu1.add(item3);
-        subMenu1.add(item9);
-
-        JMenu subMenu2 = new JMenu("Edit");
-        JMenuItem item4 = new JMenuItem("Cut");
-        JMenuItem item5 = new JMenuItem("Copy");
-        JMenuItem item6 = new JMenuItem("Paste");
-        subMenu2.add(item4);
-        subMenu2.add(item5);
-        subMenu2.add(item6);
-
-        JMenuItem closeMenu = new JMenuItem("Close");
-        closeMenu.addActionListener(this);
-
-        menuBar.add(item1);
-        menuBar.add(item2);
-        menuBar.add(closeMenu);
-
-        frame.setJMenuBar(menuBar);
-        frame.add(textArea);
-        JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(600, 600));
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public void actionPerformed(ActionEvent event){
-        String actionString = event.getActionCommand();
-
-        if (actionString.equals("Cut")) {
-            textArea.cut();
-        }
-        else if (actionString.equals("Copy")){
-            textArea.copy();
-        }
-        else if (actionString.equals("Paste")){
-            textArea.paste();
-        }
-        else if (actionString.equals("Save")){
-            JFileChooser chsr = new JFileChooser("f:");
-            int sDialog = chsr.showSaveDialog(null);
-
-            if (sDialog == JFileChooser.APPROVE_OPTION) {
-                File file = new File(chsr.getSelectedFile().getAbsolutePath());
-
-                try {
-                    FileWriter wr = new FileWriter(file, false);
-
-                    BufferedWriter bw = new BufferedWriter(wr);
-
-                    bw.write(textArea.getText());
-                    bw.flush();
-                    bw.close();
-                }catch (Exception evnt){
-                    JOptionPane.showMessageDialog(frame, evnt.getMessage());
-                }
-
-            }
-            else {
-                JOptionPane.showMessageDialog(frame, "User cancelled operation");
-            }
-        }
-        else if (actionString.equals("Print")){
-            try{
-                textArea.print();
-            }catch (Exception evnt){
-                JOptionPane.showMessageDialog(frame, evnt.getMessage());
-            }
-        }
-        else if (actionString.equals("Open")){
-            JFileChooser iChooser = new JFileChooser("f:");
-
-            int oDialog = iChooser.showOpenDialog(null);
-
-            if (oDialog == JFileChooser.APPROVE_OPTION){
-                File iFile = new File(iChooser.getSelectedFile().getAbsolutePath());
-
-                try{
-                    String sb = "", se = "";
-                    FileReader fr = new FileReader(iFile);
-                    BufferedReader br = new BufferedReader(fr);
-                    se = br.readLine();
-
-                    while ((sb = br.readLine()) != null){
-                        se = se + "\n" + sb;
-                    }
-
-                    textArea.setText(se);
-                }catch (Exception evnt){
-                    JOptionPane.showMessageDialog(frame, evnt.getMessage());
-                }
-            }
-            else{
-                JOptionPane.showMessageDialog(frame, "User cancelled operation");
-            }
-        }
-        else if (actionString.equals("New")){
-            textArea.setText("");
-        }
-        else if (actionString.equals("Close")){
-            frame.setVisible(false);
-            frame.dispose();
-        }
+        HBox hbox = new HBox(area);
+        Scene scene = new Scene(hbox, 400, 400);
+        stage.setTitle("McNote");
+        stage.setScene(scene);
+        stage.show();
     }
 }
